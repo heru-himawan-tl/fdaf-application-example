a2b4230d830e2907fd3e5e49a5d4e4d6d3d3f02b
Heru Himawan Tejo Laksono <heru.htl.83@gmail.com>
2022-03-13 11:24:13 +0700

    Routine Code Update Sun 13 Mar 2022 11:24:13 AM WIB

--- fdaf-application-example/fdaf/compilable-source/fdaf-thorntail/fdaf-base/src/main/java/fdaf/base/ApplicationIdentifier.java
+++ fdaf-application-example/fdaf/compilable-source/fdaf-thorntail/fdaf-base/src/main/java/fdaf/base/ApplicationIdentifier.java
@@ -65,6 +65,6 @@ public abstract class ApplicationIdentifier extends FrameworkIdentifier {
     }
     
     public String getApplicationCompiledDate() {
-        return "2022-03-13 at 08:51:48 WIB";
+        return "2022-03-13 at 11:21:51 WIB";
     }
 }
